0fc7612c044d951caa9b65dd977f9b6fa194130f
jamnjon <mendelsonja@gmail.com>
2020-10-12 20:44:20 -0700

    no cors restrictions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
@@ -8,12 +8,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
-  allow do
-    origins 'https://10athletes.netlify.app/rankings'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
 end
