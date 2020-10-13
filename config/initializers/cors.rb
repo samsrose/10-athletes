1993bacd857a82b060dc8dae5ceecf35cc5d9eed
jamnjon <mendelsonja@gmail.com>
2020-10-13 16:18:08 -0700

    add headers to requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 
+  allow do
+    origins 'https://10athletes.netlify.app/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
