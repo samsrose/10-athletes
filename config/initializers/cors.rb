53c6dc1915b7d6db69cd41b31a6ff31fe8b4d7d1
jamnjon <mendelsonja@gmail.com>
2020-10-19 15:03:29 -0700

    added origins for sub-urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,33 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://10athletes.netlify.app'
+    origins 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origins 'https://10athletes.netlify.app/logged_in'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origins 'https://10athletes.netlify.app/rankings'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origins 'https://10athletes.netlify.app/sports'
 
     resource '*',
       headers: :any,
