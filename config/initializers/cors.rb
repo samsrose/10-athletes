b6e3f9951eaf667f2ab00b1142a73e4a0cc8b3c8
jamnjon <mendelsonja@gmail.com>
2020-10-19 14:22:03 -0700

    removed slash in origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://10athletes.netlify.app/'
+    origins 'https://10athletes.netlify.app'
 
     resource '*',
       headers: :any,
