09684b3adeb2f199d24003be95b2558d54d9a77b
jamnjon <mendelsonja@gmail.com>
2020-10-19 17:07:40 -0700

    adjust domain for production in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_10-athletes', domain: '10a-heroku.com'
+  Rails.application.config.session_store :cookie_store, key: '_10-athletes', domain: 'https://tenathletes.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_10-athletes' 
+  Rails.application.config.session_store :cookie_store, key: '_10-athletes'
 end
