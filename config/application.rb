5ffed3740ebed9c2a009e583b38bc673e3d3114e
jamnjon <mendelsonja@gmail.com>
2020-11-06 16:42:35 -0800

    add require hash keys to app.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'active_support/core_ext/hash/keys'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
