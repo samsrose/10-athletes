72e6353a8ef89d3b37a4c713a32fd39deb00960f
jamnjon <mendelsonja@gmail.com>
2020-11-06 16:21:07 -0800

    adding requirement in puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,8 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 require 'puma'
+require 'active_support/core_ext/hash/keys'
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
